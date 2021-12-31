442a1f83e4e1fca4a80e0d779304ceac97d76237
Charlotte <charlotte.brimont@hotmail.fr>
2021-12-31 17:54:13 +0100

    Premier commit

---
+++ InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/Orientation.java
@@ -0,0 +1,19 @@
+package fr.dauphine.JavaAvance.Components;
+
+import java.util.HashMap;
+
+/**
+ * 
+ * Orientation of the piece enum
+ * 
+ */
+public enum Orientation {
+	/* Implement all the possible orientations and 
+	 *  required methods to rotate
+	 */
+
+	NORTH,
+	EAST,
+	WEST,
+	SOUTH;
+}
