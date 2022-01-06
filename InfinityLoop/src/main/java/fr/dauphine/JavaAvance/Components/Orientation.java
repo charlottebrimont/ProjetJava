9bfb2e19f6e83604e227972b876514cdb45129b6
Charlotte <charlotte.brimont@hotmail.fr>
2022-01-06 13:25:30 +0100

    package component

--- InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/Orientation.java
+++ InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/Orientation.java
@@ -12,9 +12,51 @@ public enum Orientation {
 	 *  required methods to rotate
 	 */
 
-
-	NORTH,
-	EAST,
-	WEST,
-	SOUTH;
+	NORTH(0) {
+		@Override
+		public Orientation turn90() {
+			return EAST;
+		}
+	},
+	EAST(1) {
+		@Override
+		public Orientation turn90() {
+			return SOUTH;
+		}
+	},
+	SOUTH(2) {
+		@Override
+		public Orientation turn90() {
+			return WEST;
+		}
+	},
+	WEST(3) {
+		@Override
+		public Orientation turn90() {
+			return NORTH;
+		}
+	};
+	
+	private int value;
+	
+	private Orientation(int val) {
+		this.value = val;
+	}
+	
+	abstract public Orientation turn90();
+	
+	public static Orientation getOrifromValue(int val) {
+		switch(val) {
+		case 0:
+			return NORTH;
+		case 1:
+			return EAST;
+		case 2:
+			return SOUTH;
+		case 3:
+			return WEST;
+		default:
+			throw(new IllegalArgumentException("The value " + val + " isn't associated to any orientation !"));
+		}
+	}
 }
--- InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
+++ InfinityLoop/src/main/java/fr/dauphine/JavaAvance/Components/PieceType.java
@@ -11,12 +11,185 @@ import java.util.LinkedList;
  */
 public enum PieceType {
 	// Each Type has a number of connectors and a specific value
-	
 
-	VOID,
-	ONECONN,
-	BAR,
-	TTYPE,
-	FOURCONN,
-	LTYPE;
+	VOID(0, 0) {
+		@Override
+		public Orientation getOrientation(Orientation o) {
+			if (o == Orientation.NORTH) {
+				return o;
+			}
+			throw (new IllegalArgumentException("This piece can't have this orientation !"));
+		}
+		
+		@Override
+		public LinkedList<Orientation> setConnectorsList(Orientation o) {
+			return new LinkedList<Orientation>();
+		}
+		
+		@Override
+		public ArrayList<Orientation> getListOfPossibleOri() {
+			ArrayList<Orientation> ori = new ArrayList<Orientation>();
+			ori.add(Orientation.NORTH);
+			return ori;
+		}
+	},
+	
+	ONECONN(1, 1) {
+		@Override
+		public Orientation getOrientation(Orientation o) {
+			return o;
+		}
+		
+		@Override
+		public LinkedList<Orientation> setConnectorsList(Orientation o) {
+			LinkedList<Orientation> conns = new LinkedList<Orientation>();
+			conns.add(o);
+			return conns;
+		}
+		
+		@Override
+		public ArrayList<Orientation> getListOfPossibleOri() {
+			ArrayList<Orientation> ori = new ArrayList<Orientation>();
+			ori.add(Orientation.NORTH);
+			ori.add(Orientation.EAST);
+			ori.add(Orientation.SOUTH);
+			ori.add(Orientation.WEST);
+			return ori;
+		}
+	},
+	
+	BAR(2, 2) {
+		@Override
+		public Orientation getOrientation(Orientation o) {
+			if (o == Orientation.NORTH || o == Orientation.EAST) {
+				return o;
+			}
+			throw (new IllegalArgumentException("This piece can't have this orientation !"));
+		}
+		
+		@Override
+		public LinkedList<Orientation> setConnectorsList(Orientation o) {
+			LinkedList<Orientation> conns = new LinkedList<Orientation>();
+			conns.add(o);
+			conns.add(o.turn90().turn90());
+			return conns;
+		}
+		
+		@Override
+		public ArrayList<Orientation> getListOfPossibleOri() {
+			ArrayList<Orientation> ori = new ArrayList<Orientation>();
+			ori.add(Orientation.NORTH);
+			ori.add(Orientation.EAST);
+			return ori;
+		}
+	},
+	
+	TTYPE(3, 3) {
+		@Override
+		public Orientation getOrientation(Orientation o) {
+			return o;
+		}
+		
+		@Override
+		public LinkedList<Orientation> setConnectorsList(Orientation o) {
+			LinkedList<Orientation> conns = new LinkedList<Orientation>();
+			conns.add(o);
+			conns.add(o.turn90());
+			conns.add(o.turn90().turn90().turn90());
+			return conns;
+		}
+		
+		@Override
+		public ArrayList<Orientation> getListOfPossibleOri() {
+			ArrayList<Orientation> ori = new ArrayList<Orientation>();
+			ori.add(Orientation.NORTH);
+			ori.add(Orientation.EAST);
+			ori.add(Orientation.SOUTH);
+			ori.add(Orientation.WEST);
+			return ori;
+		}
+	},
+	
+	FOURCONN(4, 4) {
+		@Override
+		public Orientation getOrientation(Orientation o) {
+			if (o == Orientation.NORTH) {
+				return o;
+			}
+			throw (new IllegalArgumentException("This piece can't have this orientation !"));
+		}
+		
+		@Override
+		public LinkedList<Orientation> setConnectorsList(Orientation o) {
+			LinkedList<Orientation> conns = new LinkedList<Orientation>();
+			conns.add(Orientation.NORTH);
+			conns.add(Orientation.SOUTH);
+			conns.add(Orientation.EAST);
+			conns.add(Orientation.WEST);
+			return conns;
+		}
+		
+		@Override
+		public ArrayList<Orientation> getListOfPossibleOri() {
+			ArrayList<Orientation> ori = new ArrayList<Orientation>();
+			ori.add(Orientation.NORTH);
+			return ori;
+		}
+	},
+	
+	LTYPE(2, 5) {
+		@Override
+		public Orientation getOrientation(Orientation o) {
+			return o;
+		}
+		
+		@Override
+		public LinkedList<Orientation> setConnectorsList(Orientation o) {
+			LinkedList<Orientation> conns = new LinkedList<Orientation>();
+			conns.add(o);
+			conns.add(o.turn90());
+			return conns;
+		}
+		
+		@Override
+		public ArrayList<Orientation> getListOfPossibleOri() {
+			ArrayList<Orientation> ori = new ArrayList<Orientation>();
+			ori.add(Orientation.NORTH);
+			ori.add(Orientation.EAST);
+			ori.add(Orientation.SOUTH);
+			ori.add(Orientation.WEST);
+			return ori;
+		}
+	};
+		
+	private final int nbConnectors;
+	private final int value;
+	
+	private PieceType(int conn, int val) {
+		this.nbConnectors = conn;
+		this.value = val;
+	}
+	
+	abstract public Orientation getOrientation(Orientation o);
+	abstract public LinkedList<Orientation> setConnectorsList(Orientation o);
+	abstract public ArrayList<Orientation> getListOfPossibleOri();
+	
+	public static PieceType getTypefromValue(int val) {
+		switch(val) {
+		case 0:
+			return VOID;
+		case 1:
+			return ONECONN;
+		case 2:
+			return BAR;
+		case 3:
+			return TTYPE;
+		case 4:
+			return FOURCONN;
+		case 5:
+			return LTYPE;
+		default:
+			throw(new IllegalArgumentException("The value " + val + " isn't associated to any piece type !"));
+		}
+	}
 }
