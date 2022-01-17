495e1e97df76c016920c81ea83e379a9a3c8678a
Charlotte <charlotte.brimont@hotmail.fr>
2022-01-17 21:57:25 +0100

    on essaye le gradn main

--- InfinityLoop/src/test/java/fr/dauphine/JavaAvance/Solve/SolverTest.java
+++ InfinityLoop/src/test/java/fr/dauphine/JavaAvance/Solve/SolverTest.java
@@ -2,8 +2,6 @@ package fr.dauphine.JavaAvance.Solve;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileNotFoundException;
-
 import org.junit.Test;
 
 import fr.dauphine.JavaAvance.Components.Piece;
@@ -12,7 +10,7 @@ import fr.dauphine.JavaAvance.GUI.Grid;
 
 public class SolverTest {
 	@Test
-	public void testSolv() throws FileNotFoundException {
+	public void testSolv() {
 		Grid g = new Grid(50, 50);
 		Generator.generateLevel("test1.txt", g);
 		g = new Grid("test1.txt");
@@ -23,14 +21,14 @@ public class SolverTest {
 	}
 	
 	@Test
-	public void testPasSolv() throws FileNotFoundException {
+	public void testPasSolv() {
 		Grid g = new Grid(5, 5);
 		Generator.setGrid(g);
 		Generator.generateCC();
 		g.setPiece(0,0, new Piece(0,0 , PieceType.BAR, PieceType.BAR.getListOfPossibleOri().get(0)));
 		
 		Solver.solve("test3.txt", g);
-	
+		
 		assertEquals(false, Checker.isSolution(g));
 	}
 
